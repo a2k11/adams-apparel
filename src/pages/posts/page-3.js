db2011c237664df12a1ce1ff393253a8d18c17cd
Adam Kaewell <akaewell@xometry.com>
2020-07-14 09:24:42 -0400

    querying data from scratch in pages

--- src/pages/posts/page-3.js
+++ src/pages/posts/page-3.js
@@ -1,10 +1,50 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../../components/layout"
 
-export default () => (
-  <Layout>
-    <h1>Hello from Page 3!</h1>
-    <Link to="/page-2">Go to page 2</Link>
-  </Layout>
-)
+const Page3 = ({ data }) => {
+  return (
+    <Layout>
+      <h1>Hello from Page 3!</h1>
+      <h3>Image File Data</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Relative Path</th>
+            <th>Size of Image</th>
+            <th>Extension</th>
+            <th>Birthtime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map(({ node }, index) => (
+            <tr key={index}>
+              <td>{node.relativePath}</td>
+              <td>{node.size}</td>
+              <td>{node.extension}</td>
+              <td>{node.birthTime}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Link to="/page-2">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export const getImageData = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          size
+          extension
+          birthTime
+        }
+      }
+    }
+  }
+`
+
+export default Page3
